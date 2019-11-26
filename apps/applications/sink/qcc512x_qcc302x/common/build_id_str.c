0c1545a2a3ffa562358400dfb5387dfdfb0f6b11
fjluo <luofengjun@vcyber.com>
2019-11-26 11:43:37 +0800

    rm some files that are deleted

--- apps/applications/sink/qcc512x_qcc302x/common/build_id_str.c
+++ apps/applications/sink/qcc512x_qcc302x/common/build_id_str.c
@@ -2,5 +2,5 @@
 
 /** This file will be re-generated on each build */
 
-const uint8 build_id_string[] = "QTIL ADK 2019-11-25 16:56:42 @1574672202";
-const uint32 build_id_number = 1574672202UL;
+const uint8 build_id_string[] = "QTIL ADK 2019-11-26 10:22:08 @1574734928";
+const uint32 build_id_number = 1574734928UL;
