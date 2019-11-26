40e1900a6956641fa6e42194ed83847eced4dc6b
fjluo <luofengjun@vcyber.com>
2019-11-26 13:59:26 +0800

    cancel track some files

--- apps/applications/sink/qcc512x_qcc302x/common/build_id_str.c
+++
@@ -1,6 +0,0 @@
-#include <hydra_types.h>
-
-/** This file will be re-generated on each build */
-
-const uint8 build_id_string[] = "QTIL ADK 2019-11-26 10:22:08 @1574734928";
-const uint32 build_id_number = 1574734928UL;
